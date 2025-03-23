911484106d9915c1ffb4dee2ed40f43c8f935ed4
Neeraj <nirajnegi2403@gmail.com>
2025-03-23 20:41:02 +0530

    Add models for comments, likes, playlists, and tweets

---
+++ src/models/playList.model.js
@@ -0,0 +1,27 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playListSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        video: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, {timestamps: true}
+)
+
+export const PlayList = mongoose.model("PlayList", playListSchema)
